a736cfa98ec85549ea865067cd55491ee66002b9
saurabh-360 <s.yadav0791@gmail.com>
2017-06-13 16:17:00 +0530

    Initial Commit

---
+++ GifteryDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GifteryDemo
+//
+//  Created by Saurabh Yadav on 13/06/17.
+//  Copyright © 2017 Saurabh Yadav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
